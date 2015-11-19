040210a0a28731f8027a03ee75503cc4edd574ad
uvsq21502322 <ghita-farnatchi@hotmail.fr>
2015-11-19 11:44:32 +0100

    ajout du projet

---
+++ fr.uvsq.coo/src/main/java/coo/g3/uvsq/fr/coo/App.java
@@ -0,0 +1,13 @@
+package coo.g3.uvsq.fr.coo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ fr.uvsq.coo/src/main/java/fr/uvsq/coo/ex3_8/Personnel.java
@@ -0,0 +1,11 @@
+package fr.uvsq.coo.ex3_8;
+
+public class Personnel {
+String nom;
+String prenom;
+String date_naissance;
+
+	
+	
+	
+}
